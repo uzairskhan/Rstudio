6f3a328fe6616baa0c5e53ebfdd8337c26d8f350
uzair <uzair.skhan@gmail.com>
2019-04-25 10:14:27 +0530

    Apr 25, first

---
+++ 4 Data Transformation.R
@@ -0,0 +1,7 @@
+# #Data transformation
+# that you get the data in exactly the right form you need. 
+# Often you’ll need to create some new variables or summaries,
+# you just want to rename the variables or reorder the observations in order to make the data a 
+# little easier to work with
+# you how to transform your data using the dplyr package 
+# and a new dataset on flights departing New York City in 2013
